de849cad077ae184363e71a2393a18d6913c6732
Anna Pushkareva <smirnova_spb@mail.ru>
2022-11-08 13:11:35 +0300

    keyboard inputs done

--- src/hooks/useKeyboard.js
+++ src/hooks/useKeyboard.js
@@ -1,4 +1,20 @@
-import { useEffect, useState } from "react"
+import { useCallback, useEffect, useState } from "react"
+
+function actionByKey(key) {
+    const keyActionMap = {
+        KeyW: 'moveForward',
+        KeyS: 'moveBackward',
+        KeyA: 'moveLeft',
+        KeyD: 'moveRight',
+        Space: 'jump',
+        Digit1: 'dirt',
+        Digit2: 'grass',
+        Digit3: 'glass',
+        Digit4: 'wood',
+        Digit5: 'log',
+    }
+    return keyActionMap[key]
+}
 
 export const useKeyboard = () => {
     const [actions, setActions] = useState ({
@@ -14,6 +30,30 @@ export const useKeyboard = () => {
         texture5: false,
     })
 
+    const handleKeyDown = useCallback((e) => {
+        const action = actionByKey(e.code)
+        if(action) {
+            setActions((prev) => {
+                return ({
+                    ...prev,
+                    [action]: true
+                })
+            })
+        }
+    }, [])
+
+    const handleKeyUp = useCallback(() => {
+        const action = actionByKey(e.code)
+        if(action) {
+            setActions((prev) => {
+                return ({
+                    ...prev,
+                    [action]: false
+                })
+            })
+        }
+    }, [])
+
     useEffect(() => {
         document.addEventListener('keydown', handleKeyDown)
         document.addEventListener('keyup', handleKeyUp)
@@ -21,6 +61,7 @@ export const useKeyboard = () => {
             document.removeEventListener('keydown', handleKeyDown)
             document.removeEventListener('keyup', handleKeyUp)
         }
-    }, [])
+    }, [handleKeyDown, handleKeyUp])
 
+    return actions;
 }
